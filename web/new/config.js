210270760a87226a7835cb8ba5223d3684839023
Keeeeeeeks <gof3@cornell.edu>
2026-05-02 15:34:56 -0400

    change vercel config

--- web/new/config.js
+++ web/new/config.js
@@ -1,4 +1,4 @@
 window.AIPS_CONFIG = window.AIPS_CONFIG || {
-  backendBaseUrl: "http://46.224.11.168:8765",
-  mediaBaseUrl: "http://46.224.11.168:8765"
+  backendBaseUrl: "https://muzaik.vercel.app",
+  mediaBaseUrl: "https://muzaik.vercel.app"
 };
